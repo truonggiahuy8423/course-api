6efccd68af37a41a819994823abc8e4b28de02e8
lam <doquang227@gmail.com>
2024-11-10 12:40:35 +0700

    fix delete user

--- src/main/java/com/example/course/repository/RoleUserRepository.java
+++ src/main/java/com/example/course/repository/RoleUserRepository.java
@@ -1,11 +1,21 @@
 package com.example.course.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.course.entity.Role;
 import com.example.course.entity.RoleUser;
 import com.example.course.entity.User;
 
+@Repository
 public interface RoleUserRepository extends JpaRepository<RoleUser, Long> {
     boolean existsByUserAndRole(User user, Role role);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM role_user WHERE user_id = :userId", nativeQuery = true)
+    void deleteByUserId(@Param("userId") Long userId);
 }
