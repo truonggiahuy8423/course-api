870edb4543c30731f969bfe0e598056f761090ee
truonggiahuy8423 <134354507+truonggiahuy8423@users.noreply.github.com>
2024-11-10 23:44:16 +0700

    a

--- src/main/java/com/example/course/controller/SubjectController.java
+++ src/main/java/com/example/course/controller/SubjectController.java
@@ -1,8 +1,8 @@
 package com.example.course.controller;
 
-import com.example.course.dto.SubjectDTO;
+//import com.example.course.dto.SubjectDTO;
 import com.example.course.dto.response.AppResponse;
-import com.example.course.service.Subject.SubjectService;
+//import com.example.course.service.Subject.SubjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +17,10 @@ import java.util.List;
 @RequestMapping("/api/subjects")
 public class SubjectController {
 
-    private final SubjectService subjectService;
+//    private final SubjectService subjectService;
 
-    @GetMapping
-    public ResponseEntity<?> getAllSubjects() {
-        return ResponseEntity.ok(subjectService.getAllSubjects());
-    }
+//    @GetMapping
+//    public ResponseEntity<?> getAllSubjects() {
+//        return ResponseEntity.ok(subjectService.getAllSubjects());
+//    }
 }
