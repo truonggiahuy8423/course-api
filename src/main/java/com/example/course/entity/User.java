30a6d997992f7e304b26153aa5ef5b26fbbc4867
sunsun1102 <sonhoang880@gmail.com>
2024-12-01 20:55:05 +0700

    fix infor course/info && product

--- src/main/java/com/example/course/entity/User.java
+++ src/main/java/com/example/course/entity/User.java
@@ -23,7 +23,7 @@ public class User {
     @Column(name = "user_id", columnDefinition = "BIGINT")
     private Long userId;
 
-    @Column(name = "usernae", columnDefinition = "VARCHAR(50)")
+    @Column(name = "username", columnDefinition = "VARCHAR(50)")
     private String username;
 
     @Column(name = "email", columnDefinition = "VARCHAR(50)")
@@ -62,8 +62,8 @@ public class User {
     @Column(name = "avatar", columnDefinition = "LONGBLOB")
     private byte[] avatar;
 
-    @Column(name = "balance", columnDefinition = "BIGINT default 0L")
-    private Long balance;
+    // @Column(name = "balance", columnDefinition = "BIGINT default 0L")
+    // private Long balance;
 
     // Relation "One"
     @JsonIgnore
